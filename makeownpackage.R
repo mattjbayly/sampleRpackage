44aa8fa9b99c0f684dead54a89c1d237d8121eb8
mattjbayly <bayly@biodiversity.ubc.ca>
2016-07-26 12:10:56 -0700

    first commit check connection

---
+++ makeownpackage.R
@@ -0,0 +1,4 @@
+# sample project make your own R package through GitHUB
+# following this tutorial 
+# http://www.r-bloggers.com/create-r-package-rstudio-github-devtools/
+####################################################################
