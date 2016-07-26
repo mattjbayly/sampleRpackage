96670c236ddd50a6cebd9fde6cee11c86372f234
Matthew Bayly <bayly@biodiversity.ubc.ca>
2016-07-26 12:19:17 -0700

    Delete makeownpackage.R

--- makeownpackage.R
+++
@@ -1,4 +0,0 @@
-# sample project make your own R package through GitHUB
-# following this tutorial 
-# http://www.r-bloggers.com/create-r-package-rstudio-github-devtools/
-####################################################################
